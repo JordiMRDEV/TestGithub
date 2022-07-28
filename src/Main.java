ee88975e58fbbcc6c8f18d48d4c806e5b239c83f
Marc Picas <marc.picash@gmail.com>
2022-07-28 20:21:38 +0200

    Cambio de Marc

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");/*comment hello world*/
         System.out.println("Nueva linea");
+        System.out.println("Cambio Marc");
     }
 }
